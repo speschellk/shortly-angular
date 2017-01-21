022a4b2d7150f20225e7236b7c3d54f5f95e8f88
speschellk <kaychristensen@gmail.com>
2017-01-20 17:32:40 -0800

    Add some controller info

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,13 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+
+  // shorten link before add to Links array
+  
+  $scope.Links = [];
+
+  // $scope.addLink = () => {
+  //   $scope.Links.push($scope.longLink);
+
+  // };
 });
